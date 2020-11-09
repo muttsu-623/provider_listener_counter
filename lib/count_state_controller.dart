c9f0184687ba4545e336694cdf3885bfdf1f0c88
Mutsumi <mutsumi1996@gmail.com>
2020-11-09 22:17:03 +0900

    Fix: from StateController to StateNotifier

--- lib/count_state_controller.dart
+++ lib/count_state_controller.dart
@@ -1,7 +1,7 @@
-import 'package:hooks_riverpod/all.dart';
 import 'package:provider_listener_counter/count_state.dart';
+import 'package:state_notifier/state_notifier.dart';
 
-class CountStateController extends StateController<CountState> {
+class CountStateController extends StateNotifier<CountState> {
   CountStateController(CountState state) : super(state);
 
   void increment() {
